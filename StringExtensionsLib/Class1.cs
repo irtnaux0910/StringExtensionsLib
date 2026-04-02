cc6ca8848bdddce61619ac7f7ae477edb1c113cd
Nguyen <xnguye28@my.centennialcollege.ca>
2026-04-02 10:10:12 -0400

    Add project files.

---
+++ StringExtensionsLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace StringExtensionsLib;
+
+public static class StringExtensions
+{
+    public static bool StartsWithUpper(this string  ? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+        {
+            return false;
+        }
+        return char.IsUpper(str[0]);
+    }
+}
